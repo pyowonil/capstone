a47da0d44a3b41370461c68d6f87e13e3bf359ca
yeahgee11 <yeahgeeis@gmail.com>
2015-11-27 17:14:35 +0900

    tmply commit

--- VisualWifi/app/src/main/java/com/android/theold4/visualwifi/AutoConnectService.java
+++ VisualWifi/app/src/main/java/com/android/theold4/visualwifi/AutoConnectService.java
@@ -38,6 +38,40 @@ public class AutoConnectService extends Service implements Runnable {
     private List<ScanResult> mScanResult; // ScanResult List
     private ArrayList<String> mScanResultString = new ArrayList<String>();
 
+    public void onCreate(){
+        super.onCreate();
+
+        Log.i(TAG,"Autostart");
+        wifimanager = (WifiManager) getSystemService(WIFI_SERVICE);
+        Log.d(TAG, "Setup WIfiManager getSystemService");
+
+        // if WIFIEnabled
+        if (wifimanager.isWifiEnabled() == false)
+            wifimanager.setWifiEnabled(true);
+
+        String ssid = "dantalkbang";
+        String password = "an453f4956";
+        String capa = "WPA2";
+        System.out.println("[자동연결 시도] " + "단톡방" + " " + password);
+
+        connectWifi(ssid, password, capa);
+
+        //initWIFIScan();
+        // init WIFISCAN
+        /*scanCount = 0;
+        text = "";
+        final IntentFilter filter = new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        registerReceiver(mReceiver, filter);
+        wifimanager.startScan();
+        Log.d(TAG, "initWIFIScan()");
+        */
+
+       // getWIFIScanResult();
+
+        Log.i(TAG, "Autorun()");
+    }
 
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -60,30 +94,6 @@ public class AutoConnectService extends Service implements Runnable {
         }
     }
 
-    public void getWIFIScanResult() {
-
-        mScanResult = wifimanager.getScanResults(); // ScanResult
-
-        Collections.sort(mScanResult, new ScanResultComparator());
-
-        // Scan count
-        int scansize = mScanResult.size();
-        for (ScanResult result : mScanResult) {
-            if (result.level < -80) continue;
-
-            String password = "an453f4956";
-            connectWifi(result.SSID, password, result.capabilities);
-        }
-    }
-
-    /*private boolean connectWifi(ScanResult result) {
-        // TODO result의 password 알아내는 방법 구하기
-        // 만약 wifi가 비밀번호가 필요한데 password를 모르면 return false
-        // 만약 wifi 비밀번호가 필요하지 않으면 아래 return 실행
-        return connectWifi(result.SSID, "tmf915419", result.capabilities.toString());
-    }*/
-
-
     public void initWIFIScan() {
         // init WIFISCAN
         scanCount = 0;
@@ -96,24 +106,20 @@ public class AutoConnectService extends Service implements Runnable {
         Log.d(TAG, "initWIFIScan()");
     }
 
-    public void onCreate(){
-        super.onCreate();
+    public void getWIFIScanResult() {
 
-        Log.i(TAG,"Autostart");
-        // 쓰레드 객체 생성 후 시작
-        myThread = new Thread(this);
-        myThread.start();
+        mScanResult = wifimanager.getScanResults(); // ScanResult
+        if (mScanResult.isEmpty()) return;
+        Collections.sort(mScanResult, new ScanResultComparator());
 
-        wifimanager = (WifiManager) getSystemService(WIFI_SERVICE);
-        Log.d(TAG, "Setup WIfiManager getSystemService");
+        for (ScanResult result : mScanResult) {
+            if (result.level < -80) continue;
 
-        // if WIFIEnabled
-        if (wifimanager.isWifiEnabled() == false)
-            wifimanager.setWifiEnabled(true);
-    }
+            String password = "an453f4956";
+            System.out.println("[자동연결 시도] " + result.SSID + " " + password);
 
-    public void printToast(String messageToast) {
-        Toast.makeText(this, messageToast, Toast.LENGTH_LONG).show();
+            connectWifi(result.SSID, password, result.capabilities);
+        }
     }
 
     public void onDestroy(){
@@ -182,29 +188,17 @@ public class AutoConnectService extends Service implements Runnable {
 
 
     //run 함수안에서 while 로 스캔 - 정렬 - open / DB검사 -  sleep2초 주기로 반복
-    public void run(){
-        Log.i(TAG,"Autorun()");
-        // 실행부 ( 예시 로그에 5초마다 카운트 )
-        // 자동연결 하는 코드
-        while(true){
-            try{
-                // TODO
-                // 자동연결 서비스 구현
+   public void run(){
 
+       //getWIFIScanResult();
 
-                Log.i(TAG, "자동연결 서비스 #" + count);
-                count++;
-
-                Thread.sleep(5000);
-            }catch(InterruptedException ex){
-                Log.e(TAG, ex.toString());
-                break;
-            }
-        }
     }
+
+    public void printToast(String messageToast) {
+        Toast.makeText(this, messageToast, Toast.LENGTH_LONG).show();
+    }
+
     public IBinder onBind(Intent arg0){
         return null;
     }
-
-
 }
