d1d7e87af06f43364f0d47bfb7092eadc4a58d4f
pyowonil <pyowonil@gmail.com>
2015-12-07 06:03:55 +0900

    ap 계산 단순화

--- project/app/src/main/java/com/capstone/theold4/visualwifi/visual_wifi_map.java
+++ project/app/src/main/java/com/capstone/theold4/visualwifi/visual_wifi_map.java
@@ -998,6 +998,7 @@ public class visual_wifi_map extends AppCompatActivity
             float EEy=0, WWy=0, SSy=0, NNy=0 , ENy=0, WNy=0, ESy=0, WSy=0; // 8방위 y좌표
             float EEd=0, WWd=0, SSd=0, NNd=0 , ENd=0, WNd=0, ESd=0, WSd=0; // 8방위까지의 max 거리
             float CCx=0, CCy=0, tempX=0, tempY=0, tempD=0;
+            int tempRSSI;
 
             double angle;
 
@@ -1024,88 +1025,24 @@ public class visual_wifi_map extends AppCompatActivity
                     String _mac = c.getString(c.getColumnIndex("MAC"));
                     CCx = c.getFloat(c.getColumnIndex("Longitude"));
                     CCy = c.getFloat(c.getColumnIndex("Latitude"));
+                    int maxRSSI = -9999;
 
-                    String[] sql2 = {"SELECT Latitude, Longitude FROM LocalData WHERE MAC = '" + _mac + "';",
-                            "SELECT Latitude, Longitude FROM WifiData WHERE MAC = '" + _mac + "';"};
+                    String[] sql2 = {"SELECT Latitude, Longitude, RSSI FROM LocalData WHERE MAC = '" + _mac + "';",
+                            "SELECT Latitude, Longitude, RSSI FROM WifiData WHERE MAC = '" + _mac + "';"};
                     for (int sq = 0; sq < 2; sq++) {
                         Cursor c2 = db_r.rawQuery(sql2[sq], null);
                         while (c2.moveToNext()) {
                             tempY = c2.getFloat(c2.getColumnIndex("Latitude"));
                             tempX = c2.getFloat(c2.getColumnIndex("Longitude"));
-
-                            // 중심으로의 상대적 위치 및 거리
-                            tempY -= CCy;
-                            tempX -= CCx;
-                            tempD = tempX * tempX + tempY * tempY;
-
-                            // 8방향  8점 잡기
-                            if (tempX == 0 && tempY == 0) { // 센터이면
-                                continue;
-                            } else {
-                                angle = Math.atan2(tempY, tempX);
-                                if (angle >= dir_WWN) { // 서 (절반)
-                                    if (tempD > WWd) {
-                                        WWd = tempD;
-                                        WWx = tempX;
-                                        WWy = tempY;
-                                    }
-                                } else if (angle >= dir_NNW) { // 북서
-                                    if (tempD > WNd) {
-                                        WNd = tempD;
-                                        WNx = tempX;
-                                        WNy = tempY;
-                                    }
-                                } else if (angle >= dir_NNE) {  // 북
-                                    if (tempD > NNd) {
-                                        NNd = tempD;
-                                        NNx = tempX;
-                                        NNy = tempY;
-                                    }
-                                } else if (angle >= dir_EEN) {  // 북동
-                                    if (tempD > ENd) {
-                                        ENd = tempD;
-                                        ENx = tempX;
-                                        ENy = tempY;
-                                    }
-                                } else if (angle >= dir_EES) {  // 동
-                                    if (tempD > EEd) {
-                                        EEd = tempD;
-                                        EEx = tempX;
-                                        EEy = tempY;
-                                    }
-                                } else if (angle >= dir_SSE) {  // 남동
-                                    if (tempD > ESd) {
-                                        ESd = tempD;
-                                        ESx = tempX;
-                                        ESy = tempY;
-                                    }
-                                } else if (angle >= dir_SSW) {  // 남
-                                    if (tempD > SSd) {
-                                        SSd = tempD;
-                                        SSx = tempX;
-                                        SSy = tempY;
-                                    }
-                                } else if (angle >= dir_WWS) {  // 남서
-                                    if (tempD > WSd) {
-                                        WSd = tempD;
-                                        WSx = tempX;
-                                        WSy = tempY;
-                                    }
-                                } else {   //  서 (나머지 절반)
-                                    if (tempD > WWd) {
-                                        WWd = tempD;
-                                        WWx = tempX;
-                                        WWy = tempY;
-                                    }
-                                }
-                            }  // if else 문, 8점 비교 대입
+                            tempRSSI = c2.getInt(c2.getColumnIndex("RSSI"));
+                            if(maxRSSI < tempRSSI) {
+                                CCx = tempX;
+                                CCy = tempY;
+                                maxRSSI = tempRSSI;
+                            }
                         } // while문 Local, Wifi 테이블 8점 잡기
                         c2.close();
                     }  // for문 end 8점 잡기 완료 ( DB 모두 검색 완료 )
-
-                    // 중점 잡기 ( AP 위치 잡기 )
-                    CCx = CCx + (NNx + SSx + EEx + WWx + WNx + WSx + ENx + ESx) / 8;
-                    CCy = CCy + (NNy + SSy + EEy + WWy + WNy + WSy + ENy + ESy) / 8;
 
                     // localDevice 테이블 업데이트하기
                     String sql3 = "UPDATE LocalDevice " +
